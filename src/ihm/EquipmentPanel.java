22564b4fcb580a29bad6efd40b3fadd12e82079e
Sta1rZ <alan.longuet@supelec.fr>
2015-10-13 22:17:17 +0200

    ,

--- src/ihm/EquipmentPanel.java
+++ src/ihm/EquipmentPanel.java
@@ -1,12 +1,16 @@
 package ihm;
 
+import security.Equipement;
+
 import com.alee.laf.panel.WebPanel;
 
 public class EquipmentPanel extends WebPanel {
 	
+	private Equipement eq;
+	
 	public EquipmentPanel(Equipement eq)
 	{
-		
+		this.eq=eq;
 	}
 
 }
